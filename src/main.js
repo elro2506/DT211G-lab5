4466d0b8029e4e87d15165326c344a5934d93a59
Elin Ronda <elro2506@student.miun.se>
2026-03-02 10:08:32 +0100

    Lagt till karta i HTML, JavaScript och CSS

--- src/main.js
+++ src/main.js
@@ -168,3 +168,13 @@ hamburger.addEventListener('click', () => {
   navLinks.classList.toggle('active');
 });
 
+
+
+/*Kartan*/
+var map = L.map('map').setView([55.8708, 12.83016], 13);
+var marker = L.marker([55.8708, 12.83016]).addTo(map);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
